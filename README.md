7b7617744a321085c9dbc64d31520d31750514f4
Emmanuel Nieto M <emmanuel.nieto.munoz@ciisa.cl>
2024-02-14 21:52:10 -0300

    Update days 02 Python

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lab-python-days-02
 Python days 02, initial journey tour
+* Día 02 de Python (martes 06 de febrero del 2024). Acaba de morir Sebastián Piñera.
+
+# Apuntes de estudio 
+
---
+++ main.py
@@ -0,0 +1,9 @@
+miVariable = 'Hola Mundo desde Python'
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+miVariable = 3.1415
+print(miVariable)
+print(miVariable)
+print(miVariable)
